655273b97876a5e7165fe7e13da7fa94bd702771
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-03-25 12:02:50 -0300

    Update visualizar.php

--- produtos/visualizar.php
+++ produtos/visualizar.php
@@ -1,8 +1,11 @@
 <?php
 /* Acessando as funções de Produtos */
 require_once "../src/funcoes-produtos.php";
+require_once "../src/funcoes-fabricantes.php";
 
 $listaDeProdutos = listarProdutos($conexao);
+
+$listaDeFabricantes = listarFabricantes($conexao);
 
 ?>
 
@@ -23,10 +26,25 @@ $listaDeProdutos = listarProdutos($conexao);
         <hr>
         <h2>Lendo e carregando todos os produtos.</h2>
 
-
-        
         <p><a class="btn btn-primary btn-sm" href="inserir.php">Inserir novo produto</a></p>
 
+        <div class="row g-1" >
+
+<?php foreach ($listaDeProdutos as $produto) { ?>
+            <div class="col-sm-6">
+                <article class="bg-body-secondary p-2 rounded-2">
+                
+                <input type="hidden" name="id" value="<?=$fabricante['id']?>">
+            
+                    <h3><?=$produto['nome']?></h3>
+                    <h4><?=$produto['id']?></h4>
+                    <p><b><?=$produto['preco']?></b></p>
+                    <p><b><?=$produto['qtde']?></b></p>        
+                </article>
+            </div>
+        
+<?php }?>
+        </div>
 
     </div>
 
